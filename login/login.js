7711cc8ab01b23dc7d916d5fbab1167b7cd8708a
gmarrang29-dev <gmarrang29@pess.cnehs.kr>
2025-12-23 09:37:48 +0900

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // /메인페이지로 이동
+    document.querySelector('.main-button').addEventListener('click', function() {
+        window.location.href = '/'; 
+    });
+});
